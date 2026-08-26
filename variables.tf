de727bc6640474f62c992506d927deb3025513ab
Nagendra Babu R <nagu7654@gmail.com>
2026-08-26 19:17:03 +0530

    terraform

--- variables.tf
+++ variables.tf
@@ -2,30 +2,23 @@ variable "component" {
     type = string
 }
 
-variable "health_check_path" {
-  default = "/health"
-}
-
-variable "port_number" {
-  default = 8080
-}
-
 variable "rule_priority" {
   default = 10
 }
 
 variable "project" {
-  type = string
+  default = "roboshop"
 }
 
 variable "environment" {
-  type = string
+  default = "dev"
 }
 
 variable "app_version" {
   type = string
+  default = "v3"
 }
 
 variable "domain_name" {
-  type = string
+  default = "nagendrababu.online"
 }
